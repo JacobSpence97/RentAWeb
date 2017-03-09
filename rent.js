3ed237852f7093cb9ffbcbcf4cae8925550a1bb0
basecamp <jspence@basecampcodingacademy.org>
2017-03-09 09:33:48 -0600

    made ifchecked function have the abilty to rent more than one item

--- rent.js
+++ rent.js
@@ -6,71 +6,73 @@ function total(price, repval) {
     console.log(final.toFixed(2));  
 }
 function ifchecked(){
+    var price = 0;
+    var repval = 0;
 if($("#dr_strange").is(':checked')){
-    var price = 8.00;
-    var repval = 25.00;
-    return  [price, repval];
-}else if($("#avatar").is(':checked')){
-    var price = 10.00;
-    var repval = 30.00;
-    return  [price, repval];
-}else if($("#bee").is(':checked')){
-    var price = 5.00;
-    var repval = 20.00;
-    return  [price, repval];
-}else if($("#cap").is(':checked')){
-    var price = 8.00;
-    var repval = 25.00;
-    return  [price, repval];
-}else if($("#bat").is(':checked')){
-    var price = 10.00;
-    var repval = 30.00;
-    return  [price, repval];
-}else if($("#wade").is(':checked')){
-    var price = 5.00;
-    var repval = 20.00;
-    return  [price, repval];
-}else if($("#hellboy").is(':checked')){
-    var price = 10.00;
-    var repval = 30.00;
-    return  [price, repval];
-}else if($("#billbo").is(':checked')){
-    var price = 10.00;
-    var repval = 30.00;
-    return  [price, repval];
-}else if($("#hulk").is(':checked')){
-    var price = 2.00;
-    var repval = 5.00;
-    return  [price, repval];
-}else if($("#tony").is(':checked')){
-    var price = 5.00;
-    var repval = 20.00;
-    return  [price, repval];
-}else if($("#it").is(':checked')){
-    var price = 2.00;
-    var repval = 5.00;
-    return  [price, repval];
-}else if($("#joke").is(':checked')){
-    var price = 10.00;
-    var repval = 30.00;
-    return  [price, repval];
-}else if($("#gandalf").is(':checked')){
-    var price = 2.00;
-    var repval = 5.00;
-    return  [price, repval];
-}else if($("#pi").is(':checked')){
-    var price = 2.00;
-    var repval = 5.00;
-    return  [price, repval];
-}else if($("#spidy").is(':checked')){
-    var price = 10.00;
-    var repval = 30.00;
-    return  [price, repval];
-}else if($("#x2").is(':checked')){
-    var price = 5.00;
-    var repval = 20.00;
-    return  [price, repval];
+    price += 8.00;
+    repval += 25.00;
+    
+}if($("#avatar").is(':checked')){
+    price += 10.00;
+    repval += 30.00;
+    
+}if($("#bee").is(':checked')){
+    price += 5.00;
+    repval += 20.00;
+    
+}if($("#cap").is(':checked')){
+    price += 8.00;
+    repval += 25.00;
+    
+} if($("#bat").is(':checked')){
+    price += 10.00;
+    repval += 30.00;
+    
+} if($("#wade").is(':checked')){
+    price += 5.00;
+    repval += 20.00;
+    
+} if($("#hellboy").is(':checked')){
+    price += 10.00;
+    repval += 30.00;
+    
+} if($("#billbo").is(':checked')){
+    price += 10.00;
+    repval += 30.00;
+    
+} if($("#hulk").is(':checked')){
+    price += 2.00;
+    repval += 5.00;
+    
+} if($("#tony").is(':checked')){
+    price += 5.00;
+    repval += 20.00;
+    
+} if($("#it").is(':checked')){
+    price += 2.00;
+    repval += 5.00;
+    
+} if($("#joke").is(':checked')){
+    price += 10.00;
+    repval += 30.00;
+    
+} if($("#gandalf").is(':checked')){
+    price += 2.00;
+    repval += 5.00;
+    
+} if($("#pi").is(':checked')){
+    price += 2.00;
+    repval += 5.00;
+    
+} if($("#spidy").is(':checked')){
+    price += 10.00;
+    repval += 30.00;
+    
+} if($("#x2").is(':checked')){
+    price += 5.00;
+    repval += 20.00;
 }
+return  [price, repval];
 }
 $( document ).ready(function(){
     $("#submit").click(function(){
